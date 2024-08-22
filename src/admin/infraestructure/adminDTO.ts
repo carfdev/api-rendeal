9af574c6caae3ecbacc739a11644ee54d0332511
carfdev <jose.carlos@fdez.se>
2024-08-22 15:30:38 +0200

    create createAdminDTO

--- src/admin/infraestructure/adminDTO.ts
+++ src/admin/infraestructure/adminDTO.ts
@@ -2,9 +2,20 @@ import { t } from "elysia";
 
 // Define the data transfer object (DTO) for the login request
 export const loginAdminDTO = {
-  // Define the structure of the request body
   body: t.Object({
-    email: t.String(), // Email field as a string
-    password: t.String(), // Password field as a string
+    email: t.String({
+      format: "email",
+    }), // Email field with email format validation
+    password: t.String({ minLength: 8 }), // Password field with minimum length validation
+  }),
+};
+
+// Define the data transfer object (DTO) for creating a new admin
+export const createAdminDTO = {
+  body: t.Object({
+    email: t.String({
+      format: "email",
+    }), // Email field with email format validation
+    password: t.String({ minLength: 8 }), // Password field with minimum length validation
   }),
 };
