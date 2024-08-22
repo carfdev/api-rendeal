28eb70329397c2ab2a6ddbc1563c3fde78a9adfe
carfdev <jose.carlos@fdez.se>
2024-08-22 18:53:57 +0200

    add createAdminDTO

--- src/admin/infraestructure/adminDTO.ts
+++ src/admin/infraestructure/adminDTO.ts
@@ -2,20 +2,42 @@ import { t } from "elysia";
 
 // Define the data transfer object (DTO) for the login request
 export const loginAdminDTO = {
-  body: t.Object({
-    email: t.String({
-      format: "email",
-    }), // Email field with email format validation
-    password: t.String({ minLength: 8 }), // Password field with minimum length validation
-  }),
+  body: t.Object(
+    {
+      email: t.String({
+        format: "email",
+        error: { message: "Invalid email format" },
+      }), // Email field with format validation
+      password: t.String({
+        minLength: 8,
+        error: { message: "Password must be at least 8 characters long" },
+      }), // Password field with minimum length validation
+    },
+    {
+      error: {
+        message: "Email and password are required", // General error message for missing fields
+      },
+    }
+  ),
 };
 
 // Define the data transfer object (DTO) for creating a new admin
 export const createAdminDTO = {
-  body: t.Object({
-    email: t.String({
-      format: "email",
-    }), // Email field with email format validation
-    password: t.String({ minLength: 8 }), // Password field with minimum length validation
-  }),
+  body: t.Object(
+    {
+      email: t.String({
+        format: "email",
+        error: { message: "Invalid email format" },
+      }), // Email field with format validation
+      password: t.String({
+        minLength: 8,
+        error: { message: "Password must be at least 8 characters long" },
+      }), // Password field with minimum length validation
+    },
+    {
+      error: {
+        message: "Email and password are required", // General error message for missing fields
+      },
+    }
+  ),
 };
